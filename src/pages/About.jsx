271a4675953e675633c7e31a1addbc440f8ae956
Landon Colburn <colburnlandon@gmail.com>
2021-06-10 17:42:29 -0500

    Removed unused variable

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,4 @@
 import Header from "components/Header/Header";
-import Hero from "components/Hero/Hero";
 import Section from "components/Section/Section";
 import Footer from "components/Footer/Footer";
 
