6310439be33292a5e565d745116657ea2cf59470
Landon Colburn <colburnlandon@gmail.com>
2021-06-18 00:56:39 -0500

    Mobile-ified contact form with media query

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -7,20 +7,20 @@ function ContactForm() {
 				<form action="">
 					<div className="toprow">
 						<div className="sidebyside">
-							<label for="firs">First Name</label>
+							<label htmlFor="firs">First Name</label>
 							<input type="text" name="firstname" id="firs" placeholder="Johnathan" />
 						</div>
 						
 						<div className="sidebyside">
-							<label for="last">Last Name</label>
+							<label htmlFor="last">Last Name</label>
 							<input type="text" name="lastname" id="last" placeholder="Smith" />
 						</div>
 					</div>
 					
-					<label for="mail">Email</label>
+					<label htmlFor="mail">Email</label>
 					<input type="email" name="email" id="mail" placeholder="jsmith@mail.com" />
 					
-					<label for="mess">Message</label>
+					<label htmlFor="mess">Message</label>
 					<textarea name="message" id="mess" cols="30" rows="10" placeholder="Enter your message here..."></textarea>
 					
 					<input type="submit" value="Submit" />
